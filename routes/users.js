ec6e0b58f823abe88b2d36230d4809ec2ec05801
chattavat.macOs <chattavat.v@gmail.com>
2019-08-10 21:36:16 +0700

    add user model & validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator'); 
+
+const User = require('../models/User');
 
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post('/', (req, res) => {
-  res.send('Rigister a user');
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Plase include a vlid email').isEmail(),
+  check('password', 'Plase enter a password with 6 or more characters').isLength({ min: 6})
+],(req, res) => {
+  const error = validationResult(req);
+  
+  if(!error.isEmpty()) {
+    return res.status(400).json({ error: error.array()});
+  }
+
+  res.send('passed');
 });
 
-module.exports = router; 
+module.exports = router;
