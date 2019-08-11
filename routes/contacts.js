47265c2ad8d6f28a34da39762e427d62f79b6b9c
chattavat.macOs <chattavat.v@gmail.com>
2019-08-11 18:14:07 +0700

    add contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { check, validationResult } = require('express-validator'); 
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 const Contact = require('../models/Contact');
@@ -11,7 +11,7 @@ const Contact = require('../models/Contact');
 // @access Private
 router.get('/', auth, async (req, res) => {
   try {
-    const contacts = await Contact.find({ user: req.user.id}).sort({ date: -1 });
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
     res.json(contacts);
   } catch (err) {
     console.error(err.message);
@@ -22,8 +22,32 @@ router.get('/', auth, async (req, res) => {
 // @route POST api/contacts
 // @desc Add new contact
 // @access Private
-router.post('/', (req, res) => {
-  res.send('Add new contact');
+router.post('/', [ auth, [ check('name', 'Name is required').not().isEmpty() ]
+  ], async (req, res) => {
+    const error = validationResult(req);
+  
+    if(!error.isEmpty()) {
+      return res.status(400).json({ error: error.array()});
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
 });
 
 // @route PUT api/contacts/:id
