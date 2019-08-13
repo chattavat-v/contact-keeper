03f36dd4fe5f3416fed7069eaf01bae3f00581a7
chattavat.macOs <chattavat.v@gmail.com>
2019-08-14 00:26:03 +0700

    build delete contact and use Context

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -11,11 +11,14 @@ import {
 export default (state, action) => {
   switch(action.type) {
     case ADD_CONTACT:
-      console.log(...state.contacts);
-      console.log(action.payload);
       return {
         ...state,
         contacts: [...state.contacts, action.payload]
+      }
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(contact => contact.id !== action.payload)
       }
     default:
       return state;
