31811ce558d6ce463a4db058a057586f9de674fc
chattavat.macOs <chattavat.v@gmail.com>
2019-08-17 17:20:27 +0700

    build loadUser and set token

--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -25,7 +26,24 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // load user
-  const loadUser = () => console.log('loadUser');
+  const loadUser = async () => {
+    if(localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      })
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR
+      })
+    }
+  };
 
   // register user
   const register = async formData => {
