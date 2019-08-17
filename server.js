73fded3c5950e367db909c8636d887cbed4e2d16
chattavat.macOs <chattavat.v@gmail.com>
2019-08-18 01:18:19 +0700

    refactor config

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'producion') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  });
+  );
 }
 
 const PORT = process.env.PORT || 5000;
