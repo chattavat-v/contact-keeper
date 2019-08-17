14758617f73e189d0c69761e4b254df8059188e1
chattavat.macOs <chattavat.v@gmail.com>
2019-08-18 00:16:46 +0700

    update contact with Context

--- client/src/context/contact/contactState.js
+++ client/src/context/contact/contactState.js
@@ -79,6 +79,28 @@ const ContactState = props => {
     }
   };
 
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Contact-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.responose.msg
+      });
+    }
+  };
+
   // Clear Contacts
   const clearContacts = () => {
     dispatch({
@@ -98,14 +120,6 @@ const ContactState = props => {
   const clearCurrent = () => {
     dispatch({
       type: CLEAR_CURRENT
-    })
-  };
-
-  // Update Contact
-  const updateContact = contact => {
-    dispatch({
-      type: UPDATE_CONTACT,
-      payload: contact
     })
   };
 
